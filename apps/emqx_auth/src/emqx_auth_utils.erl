e8924df29557472d1d0fb285a40a9109be22a1c9
JianBo He <heeejianbo@gmail.com>
2025-11-06 16:49:19 +0800

    chore: Add disc cache query

--- apps/emqx_auth/src/emqx_auth_utils.erl
+++ apps/emqx_auth/src/emqx_auth_utils.erl
@@ -19,13 +19,16 @@
     _Request :: term()
 ) -> term().
 cached_simple_sync_query(CacheName, CacheKey, ResourceID, Query) ->
-    emqx_auth_cache:with_cache(CacheName, CacheKey, fun() ->
+    Fun = fun() ->
         case emqx_resource:simple_sync_query(ResourceID, eval_query(Query)) of
             {error, _} = Error ->
                 {nocache, Error};
             Result ->
                 {cache, Result}
         end
+    end,
+    emqx_auth_cache:with_cache(CacheName, CacheKey, fun() ->
+        with_disc_cache(CacheName, CacheKey, Fun)
     end).
 
 %%--------------------------------------------------------------------
@@ -36,3 +39,13 @@ eval_query(Query) when is_function(Query, 0) ->
     Query();
 eval_query(Query) ->
     Query.
+
+with_disc_cache(emqx_authn_cache, CacheKey, Fun) ->
+    case erlang:function_exported(emqx_whc, authn_cached_query, 2) of
+        true ->
+            emqx_whc:authn_cached_query(CacheKey, Fun);
+        false ->
+            Fun()
+    end;
+with_disc_cache(_, _, Fun) ->
+    Fun().
